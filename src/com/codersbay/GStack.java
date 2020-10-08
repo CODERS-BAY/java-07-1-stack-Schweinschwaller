40ca7ae27ed416c960680bbe5c6e9d87104e45e9
Georg Schweinschwaller <georg.schweinschwaller@live.at>
2020-10-08 09:23:50 +0200

    Add Stack

---
+++ src/com/codersbay/GStack.java
@@ -0,0 +1,58 @@
+package com.codersbay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GStack {
+    private List<Integer> stack;
+
+    public GStack() {
+        stack = new ArrayList<>();
+    }
+
+    public void push(int input) {
+        this.stack.add(input);
+    }
+
+    public int size() {
+        return this.stack.size();
+    }
+
+    public int pop() throws StackTooSmallException {
+        if (stack.isEmpty()) {
+            throw new StackTooSmallException("pop");
+        }
+        int help = this.stack.get(this.stack.size() - 1);
+        this.stack.remove(this.stack.size() - 1);
+        return help;
+    }
+
+    public int peek() throws StackTooSmallException {
+        if (this.stack.isEmpty()) {
+            throw new StackTooSmallException("peek");
+        }
+        return this.stack.get(this.stack.size() - 1);
+    }
+
+    public int[] pop(int n) throws StackTooSmallException {
+        if (n > this.stack.size()) {
+            throw new StackTooSmallException("pop");
+        }
+        int[] help = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            help[i] = this.stack.get(this.stack.size() - 1);
+            this.stack.remove(this.stack.size() - 1);
+        }
+        return help;
+    }
+
+    @Override
+    public String toString() {
+        String help = "";
+        for (int i = 0; i < this.stack.size() - 1; i++) {
+            help += this.stack.get(i).toString() + ", ";
+        }
+        help += this.stack.get(this.stack.size() - 1).toString();
+        return help;
+    }
+}
--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,9 +1,53 @@
 package com.codersbay;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
+        GStack stack = new GStack();
+
+        //PUSH and POP TEST
+        System.out.println("push and pop test:");
+        System.out.println("push 0-9");
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        System.out.println(stack.toString());
+
+        System.out.println("pop " + (stack.size() / 2));
+        // Try bercause of the exception
+        int[] helpArray = new int[stack.size() / 2];
+        try {
+            helpArray = stack.pop(stack.size() / 2);
+        } catch (StackTooSmallException e) {
+            e.printStackTrace();
+        }
+        System.out.println(stack.toString());
+        System.out.print("return value: ");
+        System.out.println(Arrays.toString(helpArray));
+        System.out.println("pop one element");
+        int helpInt = 0;
+        try {
+            helpInt = stack.pop();
+        } catch (StackTooSmallException e) {
+            e.printStackTrace();
+        }
+        System.out.println(stack.toString());
+        System.out.println("return value: " + helpInt);
+
+        //SIZE TEST
+        System.out.println("size test");
+        System.out.println(stack.toString());
+        System.out.println("size " + stack.size());
+
+        //PEEK TEST
+        System.out.println("peek test");
+        try {
+            System.out.println("peek: " + stack.peek());
+        } catch (StackTooSmallException e) {
+            e.printStackTrace();
+        }
+
 
     }
-    
 }
